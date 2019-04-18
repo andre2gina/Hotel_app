68ab9374dda598961282e832b728ce3ad1f62dda
Andre_gina <49435055+andre2gina@users.noreply.github.com>
2019-04-18 11:17:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php get_header(); 
+if ( have_posts() ) :
+	/* Start the Loop */
+	while ( have_posts() ) : the_post(); ?>
+		<h1><?php the_title(); ?></h1>
+
+		<?php the_content(); ?>
+		<?php the_date();	?>
+		<?php the_category(); ?>			
+		<?php
+
+		echo '<hr>';
+
+				// End the loop.
+	endwhile;
+
+				// If no content, include the "No posts found" template.
+else :
+
+	echo 'nu avem posturi';
+endif;
+?>
+<?php get_footer(); ?>
